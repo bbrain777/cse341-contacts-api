1e12db2dd6b18f98c04d9cfdaf1b0c5b71dea3a7
Olakunle Obademi <oobademi@byupathway.edu>
2026-01-13 11:03:54 +0000

    W02 Complete CRUD: POST PUT DELETE + REST tests

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -36,4 +36,96 @@ const getSingleContact = async (req, res) => {
   }
 };
 
-module.exports = { getAllContacts, getSingleContact };
+const createContact = async (req, res) => {
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    // Basic required fields check
+    if (!contact.firstName || !contact.lastName || !contact.email) {
+      return res.status(400).json({
+        message: "firstName, lastName, and email are required"
+      });
+    }
+
+    const db = getDb();
+    const response = await db.collection("contacts").insertOne(contact);
+
+    if (response.acknowledged) {
+      return res.status(201).json({ id: response.insertedId });
+    }
+
+    return res.status(500).json({ message: "Failed to create contact" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const updateContact = async (req, res) => {
+  try {
+    const contactId = req.params.id;
+
+    if (!ObjectId.isValid(contactId)) {
+      return res.status(400).json({ message: "Invalid contact id" });
+    }
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const db = getDb();
+    const response = await db
+      .collection("contacts")
+      .updateOne({ _id: new ObjectId(contactId) }, { $set: contact });
+
+    if (response.matchedCount === 0) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    return res.status(204).send();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const contactId = req.params.id;
+
+    if (!ObjectId.isValid(contactId)) {
+      return res.status(400).json({ message: "Invalid contact id" });
+    }
+
+    const db = getDb();
+    const response = await db
+      .collection("contacts")
+      .deleteOne({ _id: new ObjectId(contactId) });
+
+    if (response.deletedCount === 0) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    return res.status(204).send();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+
+module.exports = {
+  getAllContacts,
+  getSingleContact,
+  createContact,
+  updateContact,
+  deleteContact
+};
+
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -4,4 +4,9 @@ const contactsController = require("../controllers/contactsController");
 router.get("/", contactsController.getAllContacts);
 router.get("/:id", contactsController.getSingleContact);
 
+// Week 2 CRUD
+router.post("/", contactsController.createContact);
+router.put("/:id", contactsController.updateContact);
+router.delete("/:id", contactsController.deleteContact);
+
 module.exports = router;
